4ca05203b84b413c0742281e7db63914234a3bcf
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-03-21 13:43:13 +0300

    Add ISortingEngine<T>

---
+++ NET.S.2019.Sakovich.01/SortingTask/SortingTask/SortingServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingTask
+{
+    public interface ISortingEngine<T>
+    {
+        void Sort(T[] array);
+    }
+}
