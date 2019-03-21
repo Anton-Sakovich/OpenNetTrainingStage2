e1931c0a3ab7f2bf5d7392995aaee79e79bd6ebc
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-03-21 15:57:34 +0300

    Add MergesortTests

---
+++ NET.S.2019.Sakovich.01/SortingTask/SortingTask.Tests/MergesortTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SortingTask.Tests
+{
+    [TestFixture]
+    public class MergesortTests : SortTestBase
+    {
+        public MergesortTests() : base(new Mergesort())
+        {
+
+        }
+
+        [Test]
+        public void LightweightRandomTests()
+        {
+            RandomTests(1000, 15, 0, 100);
+        }
+
+        [Test]
+        public void HeavyRandomTests()
+        {
+            RandomTests(1000, 10000, int.MinValue, int.MaxValue);
+        }
+    }
+}
