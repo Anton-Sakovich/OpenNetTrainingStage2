05fa0e8f5afa0e2f744634474847e669a1f5d10e
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-03-24 16:07:23 +0300

    Add IStopwatch and StopwatchTimer

---
+++ NET.S.2019.Sakovich.02/NextNumberTask/NextNumberTask/TimeMeasurementServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NextNumberTask
+{
+    public interface IStopwatch
+    {
+        void Start();
+        void Stop();
+        void Reset();
+
+        bool IsRunning { get; }
+        TimeSpan Elapsed { get; }
+    }
+}
