7a3eb1e7afb6e1c32420362178a1eb6e055d00dc
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-03-30 21:02:33 +0300

    Add DoubleAdapter

---
+++ NET.S.2019.Sakovich.03/IEEE754Task/IEEE754Task/DoubleAdapter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEEE754Task
+{
+    public class DoubleAdapter : FPointAdapter<double>
+    {
+        public DoubleAdapter() : base(new DoubleAnatomy()) { }
+
+        protected override double FlipSign(double num) => -num;
+
+        protected override ulong ProjectIntegerPart(double num) => (ulong)num;
+
+        protected override double LeftShift(double num, int steps)
+        {
+            for (int i = 0; i < steps; i++)
+                num /= 2D;
+
+            return num;
+        }
+
+        protected override double RightShift(double num, int steps)
+        {
+            for (int i = 0; i < steps; i++)
+                num *= 2D;
+
+            return num;
+        }
+    }
+}
