9fc3b71df0e1bae229082437701122b28a6f746e
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-03-31 20:59:40 +0300

    Add simple algebra tests

--- NET.S.2019.Sakovich.05/PolynomialTask/PolynomialTask.Tests/PolynomialTests.cs
+++ NET.S.2019.Sakovich.05/PolynomialTask/PolynomialTask.Tests/PolynomialTests.cs
@@ -60,5 +60,57 @@ namespace PolynomialTask.Tests
             Assert.That(Poly1.Powers, Is.EquivalentTo(new int[] { }));
             Assert.That(Poly1.MaxPower, Is.EqualTo(0));
         }
+
+        [Test]
+        public void EqualOperator_Tests()
+        {
+            Polynomial Poly1 = new Polynomial(new int[] { 0, 1, 2 }, new int[] { 1, 2, 3 });
+            Polynomial Poly2 = new Polynomial(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 });
+
+            Assert.That(Poly1 == Poly2, Is.False);
+            Assert.That(Poly1 != Poly2, Is.True);
+
+            Poly1 = new Polynomial(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 });
+            Poly2 = new Polynomial(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 });
+
+            Assert.That(Poly1 == Poly2, Is.True);
+            Assert.That(Poly1 != Poly2, Is.False);
+        }
+
+        [Test]
+        public void Add_Tests()
+        {
+            Polynomial Poly1 = new Polynomial(new int[] { 0, 1, 2 }, new int[] { 1, 2, 3 });
+            Polynomial Poly2 = new Polynomial(new int[] { 1, 2, 3 }, new int[] { 4, 5, 7 });
+
+            Polynomial Actual = Poly1 + Poly2;
+            Polynomial Expected = new Polynomial(new int[] { 0, 1, 2, 3 }, new int[] { 1, 6, 8, 7 });
+
+            Assert.That(Actual == Expected, Is.True, "Actual = <{0}>, Expected = <{1}>", Actual, Expected);
+        }
+
+        [Test]
+        public void Subtract_Tests()
+        {
+            Polynomial Poly1 = new Polynomial(new int[] { 0, 1, 2 }, new int[] { 1, 2, 3 });
+            Polynomial Poly2 = new Polynomial(new int[] { 1, 2, 3 }, new int[] { 4, 5, 7 });
+
+            Polynomial Actual = Poly1 - Poly2;
+            Polynomial Expected = new Polynomial(new int[] { 0, 1, 2, 3 }, new int[] { 1, -2, -2, -7 });
+
+            Assert.That(Actual == Expected, Is.True, "Actual = <{0}>, Expected = <{1}>", Actual, Expected);
+        }
+
+        [Test]
+        public void Times_Tests()
+        {
+            Polynomial Poly1 = new Polynomial(new int[] { 0, 1, 2 }, new int[] { 1, 2, 3 });
+            Polynomial Poly2 = new Polynomial(new int[] { 1, 2, 3 }, new int[] { 4, 5, 7 });
+
+            Polynomial Actual = Poly1 * Poly2;
+            Polynomial Expected = new Polynomial(new int[] { 1, 2, 3, 4, 5 }, new int[] { 4, 13, 29, 29, 21 });
+
+            Assert.That(Actual == Expected, Is.True, "Actual = <{0}>, Expected = <{1}>", Actual, Expected);
+        }
     }
 }
