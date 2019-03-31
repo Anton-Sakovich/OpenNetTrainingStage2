21fad8ba219a1f1cdc220418dd2244e518d22e5f
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-03-31 23:57:05 +0300

    Add comparers

---
+++ NET.S.2019.Sakovich.05/BubbleTask/BubbleTask/RowsMaxComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleTask
+{
+    public class RowsMaxComparer : RowsComparer
+    {
+        protected override int RowMap(int[] row)
+        {
+            int Max = row[0];
+
+            foreach (int elem in row)
+            {
+                if (elem > Max)
+                    Max = elem;
+            }
+
+            return Max;
+        }
+    }
+}
---
+++ NET.S.2019.Sakovich.05/BubbleTask/BubbleTask/RowsMinComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleTask
+{
+    public class RowsMinComparer : RowsComparer
+    {
+        protected override int RowMap(int[] row)
+        {
+            int Min = row[0];
+
+            foreach (int elem in row)
+            {
+                if (elem < Min)
+                    Min = elem;
+            }
+
+            return Min;
+        }
+    }
+}
