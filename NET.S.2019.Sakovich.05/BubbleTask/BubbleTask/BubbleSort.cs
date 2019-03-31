be6069406d266eceb486c449efaa88a1aaed995b
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-01 00:55:39 +0300

    Add equality check to BubbleSort
    Without the equality check, when desc=true, the algorithm will be
    non-stable.

--- NET.S.2019.Sakovich.05/BubbleTask/BubbleTask/BubbleSort.cs
+++ NET.S.2019.Sakovich.05/BubbleTask/BubbleTask/BubbleSort.cs
@@ -11,12 +11,16 @@ namespace BubbleTask
         public void Sort(int[][] array, IComparer<int[]> comp, bool desc = false)
         {
             int[] Temp;
+            int CompareResult;
 
             for (int UnsortedLength = array.Length; UnsortedLength > 1; UnsortedLength--)
             {
                 for(int BubblePos = 0; BubblePos < (UnsortedLength - 1); BubblePos++)
                 {
-                    if((comp.Compare(array[BubblePos], array[BubblePos + 1]) > 0) ^ desc)
+                    CompareResult = comp.Compare(array[BubblePos], array[BubblePos + 1]);
+
+                    // & (CompareResult != 0) is required to keep the algorithm stable when desc = true
+                    if (((CompareResult > 0) ^ desc) & (CompareResult != 0))
                     {
                         Temp = array[BubblePos];
                         array[BubblePos] = array[BubblePos + 1];
