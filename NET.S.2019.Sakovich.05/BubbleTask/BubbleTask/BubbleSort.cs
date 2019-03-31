ec7dbc448dbde8507116634383686b502c4d664f
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-03-31 23:36:32 +0300

    Add BubbleTask first draft

---
+++ NET.S.2019.Sakovich.05/BubbleTask/BubbleTask/BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleTask
+{
+    public class BubbleSort
+    {
+        public void Sort(int[][] array, IComparer<int[]> comp)
+        {
+            int[] Temp;
+
+            for (int UnsortedLength = array.Length; UnsortedLength > 1; UnsortedLength--)
+            {
+                for(int BubblePos = 0; BubblePos < (UnsortedLength - 1); BubblePos++)
+                {
+                    if(comp.Compare(array[BubblePos], array[BubblePos + 1]) > 0)
+                    {
+                        Temp = array[BubblePos];
+                        array[BubblePos] = array[BubblePos + 1];
+                        array[BubblePos + 1] = Temp;
+                    }
+                }
+            }
+        }
+    }
+}
