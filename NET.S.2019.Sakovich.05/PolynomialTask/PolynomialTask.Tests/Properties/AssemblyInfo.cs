2a99a9eaf729eed31562b3f396f3797e67b675ba
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-03-31 19:34:18 +0300

    Add an empty project for NUnit tests

---
+++ NET.S.2019.Sakovich.05/PolynomialTask/PolynomialTask.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PolynomialTask.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PolynomialTask.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3a1646e8-d20f-466c-99d8-24829b2d1a60")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
