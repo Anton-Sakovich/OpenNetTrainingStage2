cb4aca37c4e1cd2e4eea713cdc8c56fd20d601f2
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-02 23:29:51 +0300

    Optimize binary Euclid GCD method
    The new method now uses only one extra if check. No swaps, no extra
    if checks.

--- NET.S.2019.Sakovich.03/GCDTask/GCDTask/GCDBinaryEuclid.cs
+++ NET.S.2019.Sakovich.03/GCDTask/GCDTask/GCDBinaryEuclid.cs
@@ -12,35 +12,91 @@ namespace GCDTask
         {
             int Shift = 0;
 
-            while ((x > 0) && (y > 0))
+            // A this point x and y are arbitrary.
+
+            /*
+             * An important note:
+             * For an even number greater than zero, right shifting it until it
+             * becomes odd will never result in zero. For this reason, we have to
+             * explicitly exclude initial zeros.
+             */
+
+            if (x == 0)
             {
-                switch (((x & 1) << 1) | y & 1)
+                return y;
+            }
+
+            if (y == 0)
+            {
+                return x;
+            }
+
+            // A this point both x and y are greater than zero.
+            // Except of that, they are arbitrary.
+
+            // If both of them are even, make at least one of them odd.
+            // While they both are even, memorize shifts.
+            while ((x & 1 | y & 1) == 0)
+            {
+                x >>= 1;
+                y >>= 1;
+                Shift++;
+            }
+
+            // At this point at least one number in the pair is odd.
+
+            // Make the other one odd.
+            if ((x & 1) == 0)
+            {
+                while((x & 1) == 0)
                 {
-                    case 0:
-                        Shift++;
-                        x >>= 1;
-                        y >>= 1;
-                        break;
-                    case 1:
-                        x >>= 1;
-                        break;
-                    case 2:
-                        y >>= 1;
-                        break;
-                    case 3:
-                        if(x > y)
-                        {
-                            x = (x - y) >> 1;
-                        }
-                        else
-                        {
-                            y = (y - x) >> 1;
-                        }
-                        break;
+                    x >>= 1;
+                }
+            }
+            else
+            {
+                while ((y & 1) == 0)
+                {
+                    y >>= 1;
                 }
             }
 
-            return (x > 0 ? x : y) << Shift;
+            while (true)
+            {
+                // A this point both of them are odd and greater than zero (read the note above).
+                if (x > y)
+                {
+                    x -= y;
+
+                    // Only here and only x can become zero, which is the termination condition.
+                    if (x == 0)
+                    {
+                        return y << Shift;
+                    }
+
+                    // Here, y is still odd and x is even.
+                    while ((x & 1) == 0)
+                    {
+                        x >>= 1;
+                    }
+                }
+                else
+                {
+                    y -= x;
+
+                    // Only here and only y can become zero, which is the termination condition.
+                    if (y == 0)
+                    {
+                        return x << Shift;
+                    }
+
+                    // Here, x is still odd and y is even.
+                    while ((y & 1) == 0)
+                    {
+                        y >>= 1;
+                    }
+                }
+            }
         }
     }
 }
