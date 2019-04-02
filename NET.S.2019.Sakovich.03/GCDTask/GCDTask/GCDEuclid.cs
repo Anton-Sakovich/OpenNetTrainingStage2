0df31bb74b14faca819b50c7d83c84b5813754fd
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-02 23:35:22 +0300

    Explain how Euclid GCD algorithm is optimized

--- NET.S.2019.Sakovich.03/GCDTask/GCDTask/GCDEuclid.cs
+++ NET.S.2019.Sakovich.03/GCDTask/GCDTask/GCDEuclid.cs
@@ -10,7 +10,13 @@ namespace GCDTask
     {
         protected override int GetGCDBase(int min, int max)
         {
-            if(max < min)
+            /*
+             * The idea of the algorithm is that when there is max and min (max >= min),
+             * then max % min is smaller than both of them. Therefore, we always know
+             * which number is greater and which one is smaller.
+             */
+
+            if (max < min)
             {
                 if (max == 0)
                     return min;
@@ -19,10 +25,12 @@ namespace GCDTask
 
             while(true)
             {
+                // Here always max >= min
                 if (min == 0)
                     return max;
                 max = max % min;
 
+                // Here always max < min
                 if (max == 0)
                     return min;
                 min = min % max;
