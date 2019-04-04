6f9d6614f30071820f160570fe55d72eb68f6de4
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-04 17:46:55 +0300

    Rewrite BubbleTask to use delayed permutation approach

---
+++ NET.S.2019.Sakovich.05/BubbleTask/BubbleTask.Tests/ArrayExtensionsTests.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BubbleTask.Tests
+{
+    [TestFixture]
+    public class ArrayExtensionsTest
+    {
+        static IEnumerable<TestCaseData> RowsMaxTestCases
+        {
+            get
+            {
+                int[][] Actual = new int[7][];
+
+                Actual[0] = new int[] { 10, 12 }; // 0 : 12
+                Actual[1] = new int[] { 5, 8 }; // 1 : 8
+                Actual[2] = new int[] { 1, 2 }; // 2 : 2
+                Actual[3] = new int[] { 9, 2 }; // 3: 9
+                Actual[4] = new int[] { 1 }; // 4: 1
+                Actual[5] = new int[] { 45, 43 }; // 5: 45
+                Actual[6] = new int[] { 96, 57, 75 }; // 6: 96
+
+                int[][] ExpectedAsc = new int[7][];
+
+                ExpectedAsc[0] = Actual[4];
+                ExpectedAsc[1] = Actual[2];
+                ExpectedAsc[2] = Actual[1];
+                ExpectedAsc[3] = Actual[3];
+                ExpectedAsc[4] = Actual[0];
+                ExpectedAsc[5] = Actual[5];
+                ExpectedAsc[6] = Actual[6];
+
+                int[][] ExpectedDesc = new int[7][];
+
+                ExpectedDesc[0] = Actual[6];
+                ExpectedDesc[1] = Actual[5];
+                ExpectedDesc[2] = Actual[0];
+                ExpectedDesc[3] = Actual[3];
+                ExpectedDesc[4] = Actual[1];
+                ExpectedDesc[5] = Actual[2];
+                ExpectedDesc[6] = Actual[4];
+
+                yield return new TestCaseData(Actual, ExpectedAsc, ExpectedDesc);
+            }
+        }
+
+        static IEnumerable<TestCaseData> RowsMinTestCases
+        {
+            get
+            {
+                int[][] Actual = new int[7][];
+
+                Actual[0] = new int[] { 10, 12 }; // 10
+                Actual[1] = new int[] { 5, 8 }; // 5
+                Actual[2] = new int[] { 1, 2 }; // 1
+                Actual[3] = new int[] { 9, 2 }; // 2
+                Actual[4] = new int[] { 1 }; // 1
+                Actual[5] = new int[] { 45, 43 }; // 43
+                Actual[6] = new int[] { 96, 57, 75 }; // 57
+
+                int[][] ExpectedAsc = new int[7][];
+
+                ExpectedAsc[0] = Actual[2];
+                ExpectedAsc[1] = Actual[4];
+                ExpectedAsc[2] = Actual[3];
+                ExpectedAsc[3] = Actual[1];
+                ExpectedAsc[4] = Actual[0];
+                ExpectedAsc[5] = Actual[5];
+                ExpectedAsc[6] = Actual[6];
+
+                int[][] ExpectedDesc = new int[7][];
+
+                ExpectedDesc[0] = Actual[6];
+                ExpectedDesc[1] = Actual[5];
+                ExpectedDesc[2] = Actual[0];
+                ExpectedDesc[3] = Actual[1];
+                ExpectedDesc[4] = Actual[3];
+                ExpectedDesc[5] = Actual[2];
+                ExpectedDesc[6] = Actual[4];
+
+                yield return new TestCaseData(Actual, ExpectedAsc, ExpectedDesc);
+            }
+        }
+
+        static IEnumerable<TestCaseData> RowsTotalTestCases
+        {
+            get
+            {
+                int[][] Actual = new int[7][];
+
+                Actual[0] = new int[] { 5, -2 }; // 3
+                Actual[1] = new int[] { 5, 8 }; // 13 
+                Actual[2] = new int[] { 1, 2 }; // 3 
+                Actual[3] = new int[] { 9, 2 }; // 11 
+                Actual[4] = new int[] { 1 }; // 1 
+                Actual[5] = new int[] { 45, 43 }; // 88 
+                Actual[6] = new int[] { 96, 57, 75 }; // 228 
+
+                int[][] ExpectedAsc = new int[7][];
+
+                ExpectedAsc[0] = Actual[4];
+                ExpectedAsc[1] = Actual[0];
+                ExpectedAsc[2] = Actual[2];
+                ExpectedAsc[3] = Actual[3];
+                ExpectedAsc[4] = Actual[1];
+                ExpectedAsc[5] = Actual[5];
+                ExpectedAsc[6] = Actual[6];
+
+                int[][] ExpectedDesc = new int[7][];
+
+                ExpectedDesc[0] = Actual[6];
+                ExpectedDesc[1] = Actual[5];
+                ExpectedDesc[2] = Actual[1];
+                ExpectedDesc[3] = Actual[3];
+                ExpectedDesc[4] = Actual[0];
+                ExpectedDesc[5] = Actual[2];
+                ExpectedDesc[6] = Actual[4];
+
+                yield return new TestCaseData(Actual, ExpectedAsc, ExpectedDesc);
+            }
+        }
+
+        static string FormatArray(int[] array)
+        {
+            return string.Concat(array.Select(n => n.ToString() + " ").ToArray());
+        }
+
+        [Test]
+        public void PermuteTests()
+        {
+            Random RandGen = new Random(DateTime.Now.Millisecond);
+
+            for (int j = 0; j < 1000; j++)
+            {
+                int[] actual = new int[10];
+
+                for (int i = 0; i < actual.Length; i++)
+                {
+                    actual[i] = i;
+                }
+
+                int[] perm = actual.OrderBy(x => RandGen.Next()).ToArray();
+
+                int[] expected = (int[])perm.Clone();
+
+                actual.Permute(perm);
+
+                Assert.That(actual, Is.EqualTo(expected), "expected = {0}, actual = {1}", FormatArray(expected), FormatArray(actual));
+            }
+        }
+
+        [TestCase(new int[] { -2, 5, 6, 14, 0, -7 }, ExpectedResult = 14)]
+        [TestCase(new int[] { 2, -2 }, ExpectedResult = 2)]
+        [TestCase(new int[] { 4, 5, 2, -3, 53, 26, 32, -563 }, ExpectedResult = 53)]
+        public int AmateurMaxTests(int[] array)
+        {
+            return array.AmateurMax();
+        }
+
+        [TestCase(new int[] { -2, 5, 6, 14, 0, -7 }, ExpectedResult = -7)]
+        [TestCase(new int[] { 2, -2 }, ExpectedResult = -2)]
+        [TestCase(new int[] { 4, 5, 2, -3, 53, 26, 32, -563 }, ExpectedResult = -563)]
+        public int AmateurMinTests(int[] array)
+        {
+            return array.AmateurMin();
+        }
+
+        [TestCase(new int[] { -2, 5, 6, 14, 0, -7 }, ExpectedResult = 16)]
+        [TestCase(new int[] { 2, -2 }, ExpectedResult = 0)]
+        [TestCase(new int[] { 4, 5, 2, -3, 53, 26, 32, -563 }, ExpectedResult = -444)]
+        public int AmateurTotalTests(int[] array)
+        {
+            return array.AmateurTotal();
+        }
+
+        [TestCaseSource(nameof(RowsMaxTestCases))]
+        public void BubbleSortByMaxTests(int[][] actualAsc, int[][] expectedAsc, int[][] expectedDesc)
+        {
+            int[][] actualDesc = (int[][])actualAsc.Clone();
+
+            actualAsc.BubbleSortBy(arr => arr.AmateurMax());
+            actualDesc.BubbleSortBy(arr => arr.AmateurMax(), true);
+
+            Assert.That(actualAsc, Is.EqualTo(expectedAsc));
+            Assert.That(actualDesc, Is.EqualTo(expectedDesc));
+        }
+
+        [TestCaseSource(nameof(RowsMinTestCases))]
+        public void BubbleSortByMinTests(int[][] actualAsc, int[][] expectedAsc, int[][] expectedDesc)
+        {
+            int[][] actualDesc = (int[][])actualAsc.Clone();
+
+            actualAsc.BubbleSortBy(arr => arr.AmateurMin());
+            actualDesc.BubbleSortBy(arr => arr.AmateurMin(), true);
+
+            Assert.That(actualAsc, Is.EqualTo(expectedAsc));
+            Assert.That(actualDesc, Is.EqualTo(expectedDesc));
+        }
+
+        [TestCaseSource(nameof(RowsTotalTestCases))]
+        public void BubbleSortByTotalTests(int[][] actualAsc, int[][] expectedAsc, int[][] expectedDesc)
+        {
+            int[][] actualDesc = (int[][])actualAsc.Clone();
+
+            actualAsc.BubbleSortBy(arr => arr.AmateurTotal());
+            actualDesc.BubbleSortBy(arr => arr.AmateurTotal(), true);
+
+            Assert.That(actualAsc, Is.EqualTo(expectedAsc));
+            Assert.That(actualDesc, Is.EqualTo(expectedDesc));
+        }
+    }
+}
---
+++ NET.S.2019.Sakovich.05/BubbleTask/BubbleTask/ArrayExtensions.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleTask
+{
+    public static class ArrayExtensions
+    {
+        /// <summary>
+        /// Sorts the elements of an input array in the order definied by applying
+        /// a given function to each of them.
+        /// </summary>
+        /// <param name="array">An array to sort.</param>
+        /// <param name="func">A function to obtain values used for sorting.</param>
+        /// <param name="desc">A boolean which determines whether the order is descending..</param>
+        public static void BubbleSortBy(this int[][] array, Func<int[], int> func, bool desc = false)
+        {
+            // Check that the array is not null and its length is greater than zero.
+            ValidateArray(array);
+
+            // An array of values used to sort the input array.
+            int[] Map = new int[array.Length];
+            // An array of indices which are permuted by the sorting algorithm.
+            int[] Indices = new int[array.Length];
+
+            // Fill the arrays.
+            for (int i = 0; i < array.Length; i++)
+            {
+                Map[i] = func(array[i]);
+                Indices[i] = i;
+            }
+
+            // Permute indices so that Map[indices[i]] is sorted.
+            if (desc)
+            {
+                BubbleSortDsc(Map, Indices);
+            }
+            else
+            {
+                BubbleSortAsc(Map, Indices);
+            }
+
+            // Permute the input array so that at its indices[i]-th element will go to 
+            // to i-th position.
+            _Permute(array, Indices);
+        }
+
+        /// <summary>
+        /// Permutes the elements of an array so that its indices[i]-th element will go to
+        /// i-th position.
+        /// </summary>
+        /// <typeparam name="T">An underlying type of the array.</typeparam>
+        /// <param name="array">An input array.</param>
+        /// <param name="indices">An array of positions.</param>
+        public static void Permute<T>(this T[] array, int[] indices)
+        {
+            ValidateArray(array);
+
+            _Permute(array, (int[])indices.Clone());
+        }
+
+        // Make the permuatation in O(n) swaps
+        static void _Permute<T>(T[] array, int[] indices)
+        {
+            // A temporary variable for swapping.
+            T Temp;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                // If indices[i] == -1, then array[i] has already been involved in
+                // a completed cyclic shift and therefore stands in its final position.
+                if (indices[i] == -1)
+                {
+                    continue;
+                }
+
+                // If indices[i] != -1, then array[i] belongs to a cycle which must
+                // be shifted. The following code shifts the cycle to its final
+                // position, marking elements belonging to the cycle by -1 in the
+                // indices array.
+
+                int ThisPos = i;
+                int NextPos = indices[i];
+                while (NextPos != i)
+                {
+                    // Swap array[ThisPos] and array[NextPos]
+                    Temp = array[ThisPos];
+                    array[ThisPos] = array[NextPos];
+                    array[NextPos] = Temp;
+
+                    // After the swap, array[ThisPos] is in its final position.
+                    indices[ThisPos] = -1;
+
+                    // Now in the array[NextPos] slot is the element which came
+                    // from the array[ThisPos] slot. It will be the next ThisPos.
+                    ThisPos = NextPos;
+                    // The next NextPos is determined by the indices array.
+                    NextPos = indices[NextPos];
+                }
+
+                // When the shifting process is completed, the last shifted element
+                // will not be marked.
+                indices[ThisPos] = -1;
+            }
+        }
+
+        static void BubbleSortAsc(int[] map, int[] inds)
+        {
+            int Temp = 0;
+
+            for (int UnsortedLength = inds.Length; UnsortedLength > 1; UnsortedLength--)
+            {
+                for (int BubblePos = 0; BubblePos < (UnsortedLength - 1); BubblePos++)
+                {
+                    if (map[inds[BubblePos]] > map[inds[BubblePos + 1]])
+                    {
+                        Temp = inds[BubblePos];
+                        inds[BubblePos] = inds[BubblePos + 1];
+                        inds[BubblePos + 1] = Temp;
+                    }
+                }
+            }
+        }
+
+        static void BubbleSortDsc(int[] map, int[] inds)
+        {
+            int Temp = 0;
+
+            for (int UnsortedLength = inds.Length; UnsortedLength > 1; UnsortedLength--)
+            {
+                for (int BubblePos = 0; BubblePos < (UnsortedLength - 1); BubblePos++)
+                {
+                    if (map[inds[BubblePos]] < map[inds[BubblePos + 1]])
+                    {
+                        Temp = inds[BubblePos];
+                        inds[BubblePos] = inds[BubblePos + 1];
+                        inds[BubblePos + 1] = Temp;
+                    }
+                }
+            }
+        }
+
+        public static int AmateurMax(this int[] array)
+        {
+            ValidateArray(array);
+
+            int Max = array[0];
+
+            for(int i = 1; i < array.Length; i++)
+            {
+                if(array[i] > Max)
+                {
+                    Max = array[i];
+                }
+            }
+
+            return Max;
+        }
+
+        public static int AmateurMin(this int[] array)
+        {
+            ValidateArray(array);
+
+            int Min = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < Min)
+                {
+                    Min = array[i];
+                }
+            }
+
+            return Min;
+        }
+
+        public static int AmateurTotal(this int[] array)
+        {
+            ValidateArray(array);
+
+            int Total = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                Total += array[i];
+            }
+
+            return Total;
+        }
+
+        static void ValidateArray<T>(T[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("The length of the input array must be greater than zero.", nameof(array));
+            }
+        }
+    }
+}
