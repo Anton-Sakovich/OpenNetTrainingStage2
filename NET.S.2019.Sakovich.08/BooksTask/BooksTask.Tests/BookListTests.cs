3eb9e4a5c2a71c3392ddb4aaf3b6715e0e1f5021
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-08 00:09:58 +0300

    Add BooksSample and BookListTests

---
+++ NET.S.2019.Sakovich.08/BooksTask/BooksTask.Tests/BookListTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.IO;
+
+namespace BooksTask.Tests
+{
+    [TestFixture]
+    public class BookListTests
+    {
+        [Test]
+        public void SaveToFileTest()
+        {
+            BookList BookList1 = new BookList(BooksSample.Books);
+
+            string PathToBookList1 = "F:\\Temp\\BookTest1.bls";
+
+            BookList1.SaveToFile(PathToBookList1);
+
+            Assert.That(File.Exists(PathToBookList1), Is.True);
+
+            byte[] ActualBytes = File.ReadAllBytes(PathToBookList1);
+            byte[] ExpectedBytes = BooksSample.BooksBytes;
+
+            Assert.That(ActualBytes, Is.EqualTo(ExpectedBytes));
+        }
+
+        [Test]
+        public void LoadFromFileTest()
+        {
+            string PathToBookList1 = "F:\\Temp\\BookTest1.bls";
+
+            File.WriteAllBytes(PathToBookList1, BooksSample.BooksBytes);
+
+            BookList BookList1 = BookList.LoadFromFile(PathToBookList1);
+
+            Assert.That(BookList1.Books, Is.EqualTo(BooksSample.Books));
+        }
+
+        [Test]
+        public void SortBooksByTag_Price_Test()
+        {
+            BookList BookList1 = new BookList(BooksSample.Books);
+
+            BookList1.SortBooksByTag(Book.Tag.Price);
+
+            Assert.That(BookList1.Books, Is.EqualTo(BooksSample.BooksSortedByPrice));
+        }
+
+        [Test]
+        public void FindBookByTag_BookExists_TagOK_Test()
+        {
+            BookList BookList1 = new BookList(BooksSample.Books);
+
+            Book Expected = BooksSample.Books[0];
+            Book Actual = BookList1.FindBookByTag(Book.Tag.Isdbn, "123");
+            Assert.AreEqual(Actual, Expected);
+
+            Expected = BooksSample.Books[1];
+            Actual = BookList1.FindBookByTag(Book.Tag.Author, "Author2");
+            Assert.AreEqual(Actual, Expected);
+
+            Expected = BooksSample.Books[0];
+            Actual = BookList1.FindBookByTag(Book.Tag.Title, "Title1");
+            Assert.AreEqual(Actual, Expected);
+
+            Expected = BooksSample.Books[1];
+            Actual = BookList1.FindBookByTag(Book.Tag.Publisher, "Publisher2");
+            Assert.AreEqual(Actual, Expected);
+
+            Expected = BooksSample.Books[0];
+            Actual = BookList1.FindBookByTag(Book.Tag.YearPublished, 1995);
+            Assert.AreEqual(Actual, Expected);
+
+            Expected = BooksSample.Books[1];
+            Actual = BookList1.FindBookByTag(Book.Tag.Pages, 304);
+            Assert.AreEqual(Actual, Expected);
+
+            Expected = BooksSample.Books[0];
+            Actual = BookList1.FindBookByTag(Book.Tag.Price, 102);
+            Assert.AreEqual(Actual, Expected);
+        }
+
+        [Test]
+        public void FindBookByTag_TagValueMismatch_ExceptionThrown()
+        {
+            BookList BookList1 = new BookList(BooksSample.Books);
+
+            Assert.That(() => BookList1.FindBookByTag(Book.Tag.Isdbn, 123), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => BookList1.FindBookByTag(Book.Tag.Price, "Good damn it, it is a string!"), Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void FindBookByTag_BookNotFound_ExceptionThrown()
+        {
+            BookList BookList1 = new BookList(BooksSample.Books);
+
+            Assert.That(() => BookList1.FindBookByTag(Book.Tag.Isdbn, "Foo"), Throws.TypeOf<BookNotFoundException>());
+        }
+    }
+}
---
+++ NET.S.2019.Sakovich.08/BooksTask/BooksTask.Tests/BooksSample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksTask.Tests
+{
+    static class BooksSample
+    {
+        public static Book[] Books = new Book[]
+        {
+            new Book("123", "Author1", "Title1", "Publisher1", 1995, 202, 102),
+            new Book("256", "Author2", "Title2", "Publisher2", 2003, 304, 95)
+        };
+
+        public static Book[] BooksSortedByPrice = new Book[]
+        {
+            new Book("256", "Author2", "Title2", "Publisher2", 2003, 304, 95),
+            new Book("123", "Author1", "Title1", "Publisher1", 1995, 202, 102)
+        };
+
+        public static byte[] BooksBytes = new byte[]
+        {
+            3, 49, 50, 51, // "123" (BLOB)
+            7, 65, 117, 116, 104, 111, 114, 49, // "Authro1" (BLOB)
+            6, 84, 105, 116, 108, 101, 49, // "Title1" (BLOB)
+            10, 80, 117, 98, 108, 105, 115, 104, 101, 114, 49, // Publisher1 (BLOB)
+            203, 7, 0, 0, // 0x 00 00 07 CB (1995) in Little Endian
+            202, 0, 0, 0, // 0x 00 00 00 CA (202) in Little Endian
+            102, 0, 0, 0, // 0x 00 00 00 66 (102) in Little Endian
+            3, 50, 53, 54,
+            7, 65, 117, 116, 104, 111, 114, 50,
+            6, 84, 105, 116, 108, 101, 50,
+            10, 80, 117, 98, 108, 105, 115, 104, 101, 114, 50,
+            211, 7, 0, 0,
+            48, 1, 0, 0,
+            95, 0, 0, 0
+        };
+    }
+}
