1b5e3a899c407ba8718dc03c1a3d73a1cd9e285c
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-14 22:11:45 +0300

    Add FancyBookFormatterTests

---
+++ NET.S.2019.Sakovich.08/BooksTask/BooksTask.Tests/FancyBookFormatterTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BooksTask.Tests
+{
+    public class FancyBookFormatterTests
+    {
+        [Test]
+        public void Format_FormatsFancyFormatString_Test()
+        {
+            Assert.That(
+                string.Format(new FancyBookFormatter(), "{0:Fancy}", BookTests.TheArtOfTimeRewindingBook),
+                Is.EqualTo("Book(Title = \"The Art of Time Rewinding\", Author = \"Max Caulfield\""));
+        }
+
+        [Test]
+        public void Format_FallbacksToIFormattable_Test()
+        {
+            Assert.That(
+                string.Format(new FancyBookFormatter(), "{0:\\tt (\\pr)}", BookTests.TheArtOfTimeRewindingBook),
+                Is.EqualTo("The Art of Time Rewinding (205)"));
+        }
+    }
+}
