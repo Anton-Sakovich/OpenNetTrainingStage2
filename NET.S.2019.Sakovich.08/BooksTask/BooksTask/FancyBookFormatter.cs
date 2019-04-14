074f3b562007fdd08623bf2fcd7188063046ab1f
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-14 22:10:46 +0300

    Add FancyBookFormatter

---
+++ NET.S.2019.Sakovich.08/BooksTask/BooksTask/FancyBookFormatter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksTask
+{
+    public class FancyBookFormatter : IFormatProvider, ICustomFormatter
+    {
+        public object GetFormat(Type formatType)
+        {
+            if(formatType == typeof(ICustomFormatter))
+            {
+                return this;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string Format(string format, object arg, IFormatProvider provider)
+        {
+            if ((arg is Book book) && (format == "Fancy"))
+            {
+                return $"Book(Title = \"{book.Title}\", Author = \"{book.Author}\"";
+            }
+
+            return Fallback(format, arg, provider);
+        }
+
+        private string Fallback(string format, object arg, IFormatProvider provider)
+        {
+            if (arg is IFormattable formattable)
+            {
+                return formattable.ToString(format, provider);
+            }
+            else if(arg != null)
+            {
+                return arg.ToString();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
