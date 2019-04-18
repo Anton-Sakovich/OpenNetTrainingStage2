77ec665b1d7c93f4980b79edfe0e6254a90510b6
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-19 00:54:33 +0300

    Create a simple interrupt demo program

--- NET.S.2019.Sakovich.11/TimerTask/TimerTask/Program.cs
+++ NET.S.2019.Sakovich.11/TimerTask/TimerTask/Program.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TimerTask
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
+            SimpleTimer simpleTimer1 = new SimpleTimer(new TimeSpan(0, 0, 0, 10).Ticks, 1000 * 10000);
+
+            Console.CancelKeyPress += (sender, e) => simpleTimer1.Stop();
+
+            simpleTimer1.CountdownInterrupted += (sender, e) => Console.WriteLine("Interrupt! Time = {0}", TimeSpan.FromTicks(e.Ticks));
+            simpleTimer1.TimeElapsed += (sender, e) => Console.WriteLine("Finished.");
+
+            simpleTimer1.Start();
+
+            while (simpleTimer1.IsTicking)
+            {
+                Console.WriteLine(TimeSpan.FromTicks(simpleTimer1.Ticks));
+                Thread.Sleep(1000);
+            }
+
+            Console.ReadKey();
         }
     }
 }
