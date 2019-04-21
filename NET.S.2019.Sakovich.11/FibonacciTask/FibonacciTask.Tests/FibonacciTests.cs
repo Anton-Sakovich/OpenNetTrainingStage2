325c5c74aac6ee94d6239adbfad184654d8bc78c
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-21 21:32:31 +0300

    Add unit tests for Fibonacci class

---
+++ NET.S.2019.Sakovich.11/FibonacciTask/FibonacciTask.Tests/FibonacciTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciTask.Tests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        public static IEnumerable<TestCaseData> TestData
+        {
+            get
+            {
+                yield return new TestCaseData(1, new int[] { 1, 1 });
+                yield return new TestCaseData(2, new int[] { 1, 1, 2 });
+                yield return new TestCaseData(3, new int[] { 1, 1, 2, 3 });
+                yield return new TestCaseData(4, new int[] { 1, 1, 2, 3 });
+                yield return new TestCaseData(5, new int[] { 1, 1, 2, 3, 5 });
+                yield return new TestCaseData(6, new int[] { 1, 1, 2, 3, 5 });
+                yield return new TestCaseData(7, new int[] { 1, 1, 2, 3, 5 });
+                yield return new TestCaseData(8, new int[] { 1, 1, 2, 3, 5, 8 });
+                yield return new TestCaseData(9, new int[] { 1, 1, 2, 3, 5, 8 });
+            }
+        }
+
+        [TestCaseSource(nameof(TestData))]
+        public void Enumaration_Test(int max, int[] expected)
+        {
+            int[] actual = new Fibonacci(max).ToArray();
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void MoveNext_ReturnValue_Test()
+        {
+            IEnumerator<int> fib = new Fibonacci(1);
+
+            Assert.That(fib.MoveNext(), Is.True); // 1
+            Assert.That(fib.MoveNext(), Is.True); // 1
+            Assert.That(fib.MoveNext(), Is.False); // 2
+            Assert.That(fib.MoveNext(), Is.False); // 2
+        }
+
+        [Test]
+        public void InvalidBound_ArgumentExceptionThrown_Test()
+        {
+            Assert.That(() => new Fibonacci(0), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => new Fibonacci(-1), Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void InvalidCurrent_InvalidOperationExceptionThrown_Test()
+        {
+            IEnumerator<int> fib = new Fibonacci(1);
+
+            Assert.That(() => fib.Current, Throws.TypeOf<InvalidOperationException>());
+
+            fib.MoveNext(); // 1, true
+            fib.MoveNext(); // 1, true
+            fib.MoveNext(); // 2, false
+
+            Assert.That(() => fib.Current, Throws.TypeOf<InvalidOperationException>());
+        }
+    }
+}
---
+++ NET.S.2019.Sakovich.11/FibonacciTask/FibonacciTask.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FibonacciTask.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FibonacciTask.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25b1d9bb-8f34-4c86-86d5-ee9a157bf48a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
