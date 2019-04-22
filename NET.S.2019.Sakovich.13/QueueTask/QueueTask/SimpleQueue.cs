40782ca3a8e200e33f0cc44119eb432781d826e4
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-23 00:50:23 +0300

    Add first draft of SimpleQueue

--- NET.S.2019.Sakovich.13/QueueTask/QueueTask/SimpleQueue.cs
+++ NET.S.2019.Sakovich.13/QueueTask/QueueTask/SimpleQueue.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,55 @@ using System.Threading.Tasks;
 
 namespace QueueTask
 {
-    public class SimpleQueue
+    public class SimpleQueue<T> : IEnumerable<T>
     {
+        private LinkedList<T> list;
+
+        public SimpleQueue()
+        {
+            list = new LinkedList<T>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+
+        public void Enqueue(T value)
+        {
+            list.AddLast(value);
+        }
+
+        public T Peek()
+        {
+            if (list.First == null)
+            {
+                throw new InvalidOperationException("The queue is empty.");
+            }
+
+            return list.First.Value;
+        }
+
+        public T Dequeue()
+        {
+            T first = Peek();
+
+            list.RemoveFirst();
+
+            return first;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new LinkedListEnumerator<T>(list);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
