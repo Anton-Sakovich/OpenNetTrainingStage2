64dabadc0e9a8bbd707dc3afcc3d72dbaec297d2
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-23 00:50:04 +0300

    Add first draft of LinkedListEnumerator

---
+++ NET.S.2019.Sakovich.13/QueueTask/QueueTask/LinkedListEnumerator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueTask
+{
+    internal class LinkedListEnumerator<T> : IEnumerator<T>, IEnumerator
+    {
+        private LinkedList<T> list;
+        private LinkedListNode<T> current;
+        private bool currentNullMeansEnd;
+
+        public LinkedListEnumerator(LinkedList<T> list)
+        {
+            if (list == null)
+            {
+                currentNullMeansEnd = true;
+            }
+            else
+            {
+                this.list = list;
+                currentNullMeansEnd = false;
+            }
+        }
+
+        public T Current
+        {
+            get
+            {
+                if (current == null)
+                {
+                    throw new InvalidOperationException("The LinkedListEnumerator is either not initialized of completed.");
+                }
+                else
+                {
+                    return current.Value;
+                }
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (current == null)
+            {
+                if (currentNullMeansEnd)
+                {
+                    return false;
+                }
+                else
+                {
+                    currentNullMeansEnd = true;
+                    current = list.First;
+                    return current == null;
+                }
+            }
+            else
+            {
+                current = current.Next;
+                return current == null;
+            }
+        }
+
+        public void Reset()
+        {
+            currentNullMeansEnd = list == null;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
