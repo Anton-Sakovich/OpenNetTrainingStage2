2935670f0fb0294d7c43508947f9e2b194ddfaaa
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-25 19:51:20 +0300

    Add tests for SquareMatrix

---
+++ NET.S.2019.Sakovich.13/SquareMatricesTask/SquareMatricesTask.Tests/SquareMatrixTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareMatricesTask.Tests
+{
+    [TestFixture]
+    public class SquareMatrixTests
+    {
+        [Test]
+        public void Item_Test()
+        {
+            SquareMatrix<int> matrix =
+                new SquareMatrix<int>(new DiagonalSquareMatrixLayout<int>(2));
+
+            Assert.That(matrix[0, 0], Is.EqualTo(0));
+            Assert.That(matrix[0, 1], Is.EqualTo(0));
+            Assert.That(matrix[1, 0], Is.EqualTo(0));
+            Assert.That(matrix[1, 1], Is.EqualTo(0));
+            Assert.That(matrix.Layout, Is.TypeOf<DiagonalSquareMatrixLayout<int>>());
+
+            matrix[0, 0] = 1;
+            matrix[1, 1] = 2;
+
+            Assert.That(matrix[0, 0], Is.EqualTo(1));
+            Assert.That(matrix[0, 1], Is.EqualTo(0));
+            Assert.That(matrix[1, 0], Is.EqualTo(0));
+            Assert.That(matrix[1, 1], Is.EqualTo(2));
+            Assert.That(matrix.Layout, Is.TypeOf<DiagonalSquareMatrixLayout<int>>());
+
+            matrix[0, 1] = 100;
+
+            Assert.That(matrix[0, 0], Is.EqualTo(1));
+            Assert.That(matrix[0, 1], Is.EqualTo(100));
+            Assert.That(matrix[1, 0], Is.EqualTo(0));
+            Assert.That(matrix[1, 1], Is.EqualTo(2));
+            Assert.That(matrix.Layout, Is.TypeOf<SquareMatrixLayout<int>>());
+        }
+
+        [Test]
+        public void MatrixElementChanged_Test()
+        {
+            object eventSender = null;
+            MatrixElementChangedEventArgs eventArgs = null;
+
+            SquareMatrix<int> matrix = new SquareMatrix<int>(2);
+
+            matrix.MatrixElementChanged += (sender, e) => { eventSender = sender; eventArgs = e; };
+
+            matrix[0, 1] = 1;
+
+            Assert.That(eventSender, Is.EqualTo(matrix));
+            Assert.That(eventArgs?.Row, Is.EqualTo(0));
+            Assert.That(eventArgs?.Col, Is.EqualTo(1));
+        }
+    }
+}
