c734c4c8f29d7ef6b2108d6fd61e50c2ec1859a7
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-27 19:34:19 +0300

    Initialize BSTTask

---
+++ NET.S.2019.Sakovich.13/BSTTask/BSTTask/BSTNode.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTTask
+{
+    public class BSTNode<TKey, TValue>
+    {
+        public BSTNode()
+            : this(Comparer<TKey>.Default)
+        {
+        }
+
+        public BSTNode(IComparer<TKey> comparer)
+        {
+            this.Comparer = comparer;
+        }
+
+        public BSTNode(TKey key, TValue value)
+            : this(Comparer<TKey>.Default)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+
+        public BSTNode(TKey key, TValue value, IComparer<TKey> comparer)
+            : this(comparer)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+
+        private BSTNode(TKey key, TValue value, BSTNode<TKey, TValue> parent)
+            : this(parent.Comparer)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+
+        public IComparer<TKey> Comparer { get; }
+
+        public TKey Key { get; }
+        public TValue Value { get; set; }
+
+        public BSTNode<TKey, TValue> Left { get; private set; }
+        public BSTNode<TKey, TValue> Right { get; private set; }
+
+        public BSTNode<TKey, TValue> Lookup(TKey key)
+        {
+            BSTNode<TKey, TValue> currentNode = this;
+            int compareResult;
+
+            while (currentNode != null)
+            {
+                compareResult = this.Comparer.Compare(key, this.Key);
+
+                if (compareResult == 0)
+                {
+                    break;
+                }
+                else if (compareResult > 0)
+                {
+                    currentNode = currentNode.Right;
+                }
+                else
+                {
+                    currentNode = currentNode.Left;
+                }
+            }
+
+            return currentNode;
+        }
+
+        public BSTNode<TKey, TValue> Add(TKey key, TValue value)
+        {
+            int compareResult = this.Comparer.Compare(key, this.Key);
+
+            if (compareResult == 0)
+            {
+                this.Value = value;
+                return this;
+            }
+            else if (compareResult > 0)
+            {
+                this.Right = this.Right?.Add(key, value) ?? new BSTNode<TKey, TValue>(key, value, this);
+                return this;
+            }
+            else
+            {
+                this.Left = this.Left?.Add(key, value) ?? new BSTNode<TKey, TValue>(key, value, this);
+                return this;
+            }
+        }
+    }
+}
