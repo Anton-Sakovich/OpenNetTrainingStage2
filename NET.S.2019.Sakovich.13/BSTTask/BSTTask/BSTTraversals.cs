8a9b8043041e7265294d47a0cfa38d1f50e6dcd0
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-28 17:35:50 +0300

    Add first draft of Inorder traversal

---
+++ NET.S.2019.Sakovich.13/BSTTask/BSTTask/BSTTraversals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTTask
+{
+    public static class BSTTraversals
+    {
+        public static IEnumerator<BSTNode<TKey, TValue>> EnumerateInorder<TKey, TValue>(this BSTNode<TKey, TValue> root)
+        {
+            Stack<BSTNode<TKey, TValue>> stack = new Stack<BSTNode<TKey, TValue>>();
+            BSTNode<TKey, TValue> current = root;
+
+            while (current != null || stack.Count > 0)
+            {
+                if (current.Left != null)
+                {
+                    stack.Push(current);
+                    current = current.Left;
+                    continue;
+                }
+
+                yield return current;
+
+                if (current.Right != null)
+                {
+                    stack.Push(current);
+                    current = current.Right;
+                    continue;
+                }
+
+                current = stack.Pop();
+            }
+        }
+    }
+}
