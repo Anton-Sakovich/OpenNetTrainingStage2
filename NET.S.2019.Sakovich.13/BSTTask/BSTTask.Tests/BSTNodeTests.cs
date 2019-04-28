7e2b06104f74ed9caf291369dc1f3a3af42eb821
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-28 17:59:04 +0300

    Add unit tests for BSTNode

---
+++ NET.S.2019.Sakovich.13/BSTTask/BSTTask.Tests/BSTNodeTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTTask.Tests
+{
+    [TestFixture]
+    public class BSTNodeTests
+    {
+        [Test]
+        public void Add_Test()
+        {
+            BSTNode<int, string> root = new BSTNode<int, string>(9, "9");
+            root.Add(4, "4");
+            root.Add(15, "15");
+            root.Add(2, "2");
+            root.Add(6, "6");
+            root.Add(11, "11");
+            root.Add(17, "17");
+
+            Assert.That(root.Value, Is.EqualTo("9"));
+            Assert.That(root.Left.Value, Is.EqualTo("4"));
+            Assert.That(root.Right.Value, Is.EqualTo("15"));
+            Assert.That(root.Left.Left.Value, Is.EqualTo("2"));
+            Assert.That(root.Left.Right.Value, Is.EqualTo("6"));
+            Assert.That(root.Right.Left.Value, Is.EqualTo("11"));
+            Assert.That(root.Right.Right.Value, Is.EqualTo("17"));
+            Assert.That(root.Left.Left.Left, Is.Null);
+            Assert.That(root.Left.Left.Right, Is.Null);
+            Assert.That(root.Left.Right.Left, Is.Null);
+            Assert.That(root.Left.Right.Right, Is.Null);
+            Assert.That(root.Right.Left.Left, Is.Null);
+            Assert.That(root.Right.Left.Right, Is.Null);
+            Assert.That(root.Right.Right.Left, Is.Null);
+            Assert.That(root.Right.Right.Right, Is.Null);
+
+            root.Add(15, "15+");
+
+            Assert.That(root.Value, Is.EqualTo("9"));
+            Assert.That(root.Left.Value, Is.EqualTo("4"));
+            Assert.That(root.Right.Value, Is.EqualTo("15+"));
+            Assert.That(root.Left.Left.Value, Is.EqualTo("2"));
+            Assert.That(root.Left.Right.Value, Is.EqualTo("6"));
+            Assert.That(root.Right.Left.Value, Is.EqualTo("11"));
+            Assert.That(root.Right.Right.Value, Is.EqualTo("17"));
+            Assert.That(root.Left.Left.Left, Is.Null);
+            Assert.That(root.Left.Left.Right, Is.Null);
+            Assert.That(root.Left.Right.Left, Is.Null);
+            Assert.That(root.Left.Right.Right, Is.Null);
+            Assert.That(root.Right.Left.Left, Is.Null);
+            Assert.That(root.Right.Left.Right, Is.Null);
+            Assert.That(root.Right.Right.Left, Is.Null);
+            Assert.That(root.Right.Right.Right, Is.Null);
+        }
+
+        [Test]
+        public void Lookup_Test()
+        {
+            int[] keys = new int[] { 9, 4, 15, 2, 6, 11, 17 };
+            string[] values = new string[] { "9", "4", "15", "2", "6", "11", "17" };
+
+            BSTNode<int, string> root = new BSTNode<int, string>(keys[0], values[0]);
+            for (int i = 1; i < keys.Length; i++)
+            {
+                root.Add(keys[i], values[i]);
+            }
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Assert.That(root.Lookup(keys[i])?.Value, Is.EqualTo(values[i]));
+            }
+
+            Assert.That(root.Lookup(-1), Is.Null);
+        }
+    }
+}
---
+++ NET.S.2019.Sakovich.13/BSTTask/BSTTask.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BSTTask.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BSTTask.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f109886b-37f0-4aca-a2af-a1304aa153b9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
