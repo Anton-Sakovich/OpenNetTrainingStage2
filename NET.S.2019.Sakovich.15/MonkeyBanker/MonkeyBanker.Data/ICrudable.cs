e2b7bd06893a483e34c2016b729e4e8b25d7a178
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-03 11:58:09 +0300

    Substitute MonkeyBanker.Data instead of MonkeyB...
    anker.Repository

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Data/ICrudable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyBanker.Data
+{
+    public interface ICrudable<T>
+    {
+        void Create(T entity);
+
+        T Read(int id);
+
+        void Update(T entity);
+
+        void Delete(int id);
+
+        IEnumerable<T> ReadAll();
+    }
+}
--- NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Data/Properties/AssemblyInfo.cs
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Data/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MonkeyBanker.Repository")]
+[assembly: AssemblyTitle("MonkeyBanker.Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MonkeyBanker.Repository")]
+[assembly: AssemblyProduct("MonkeyBanker.Data")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c27b3b95-29ef-454b-b1d1-21987c13b776")]
+[assembly: Guid("f203cf79-67f6-4a01-904a-3542898d8362")]
 
 // Version information for an assembly consists of the following four values:
 //
