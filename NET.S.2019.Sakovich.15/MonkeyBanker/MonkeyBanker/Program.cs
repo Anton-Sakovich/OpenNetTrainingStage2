1b2242b68485e72fa2854773149f69a6d3b70d74
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-04 13:26:09 +0300

    Add MonkeyBanker console application

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonkeyBanker.Data;
+using MonkeyBanker.Entities;
+using MonkeyBanker.Data.AdoNet.SQLite;
+
+namespace MonkeyBanker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICrudable<Person> peopleRepo = new PeopleSQLiteCrudEngine("Data Source = E:/Temp/MonkeyBanker.db");
+
+            foreach (Person person in peopleRepo.ReadAll())
+            {
+                Console.WriteLine("{0} {1}", person.GivenName, person.FamilyName);
+            }
+
+            Person max = peopleRepo.Read(1);
+            max.GivenName = "Maxine";
+            peopleRepo.Update(max);
+
+            Console.ReadKey();
+        }
+    }
+}
