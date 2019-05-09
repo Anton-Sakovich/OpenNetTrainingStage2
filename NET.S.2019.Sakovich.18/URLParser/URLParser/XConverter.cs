f449c90f71c5ee162c71ed5b302ea51758c59610
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-09 19:28:09 +0300

    Add XConverter

---
+++ NET.S.2019.Sakovich.18/URLParser/URLParser/XConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace URLParser
+{
+    public static class XConverter
+    {
+        public static XElement ToXElement(this URLData urlData)
+        {
+            XElement xroot = new XElement("urlAdress", new XElement("host", new XAttribute("name", urlData.Host)));
+
+            if (urlData.PathSegments.Length > 0)
+            {
+                XElement xuri = new XElement("uri");
+
+                foreach (string pathSegment in urlData.PathSegments)
+                {
+                    xuri.Add(new XElement("segment", pathSegment));
+                }
+
+                xroot.Add(xuri);
+            }
+
+            if (urlData.Parameters.Count > 0)
+            {
+                XElement xparams = new XElement("parameters");
+
+                foreach (KeyValuePair<string, string> pair in urlData.Parameters)
+                {
+                    xparams.Add(new XElement("parameter", new XAttribute("key", pair.Key), new XAttribute("value", pair.Value)));
+                }
+
+                xroot.Add(xparams);
+            }
+
+            return xroot;
+        }
+    }
+}
