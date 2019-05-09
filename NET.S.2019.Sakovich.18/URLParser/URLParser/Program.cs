eb48cb20567360bb93d39aa18d5416f80e088b82
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-09 22:31:24 +0300

    Make Main method of URLParser more readable

--- NET.S.2019.Sakovich.18/URLParser/URLParser/Program.cs
+++ NET.S.2019.Sakovich.18/URLParser/URLParser/Program.cs
@@ -29,34 +29,58 @@ namespace URLParser
 
         public static int Main(string[] args)
         {
+            // The first command line argument is the path to the input file with
+            // urls. The second command line argument is the path to the output file.
+            // So, first of all, check that there are enough arguments.
             if (args.Length < 2)
             {
                 Console.WriteLine("Two arguments are required.");
                 return NOT_ENOUGH_ARGUMENTS;
             }
 
+            // If there are at least two command line arguments, then assign the first one
+            // to input file name and the second one to output file name.
+            string inputFileName = args[0];
+            string outputFileName = args[1];
+
+            // Now, when we have a path to the input file, try to open it for reading.
             StreamReader reader = null;
             try
             {
-                reader = new StreamReader(args[0]);
+                reader = new StreamReader(inputFileName);
             }
             catch (Exception exc)
             {
-                Console.WriteLine("Cannot open " + args[0]);
+                // If we cannot even open the file, then terminate the program.
+                Console.WriteLine("Cannot open {0}", inputFileName);
                 Console.WriteLine(exc);
                 return INPUT_FILE_OPEN_FAILURE;
             }
 
-            string logfname = Path.ChangeExtension(args[0], ".log");
-            File.Delete(logfname);
-            ConfigureNLog(Path.ChangeExtension(args[0], ".log"));
+            // Now we know that the file name provided is a valid file name and we can configure
+            // NLog to use a logfile.
 
-            logger.Info("Opened {0} for reading data.", args[0]);
+            // We have waited till here before configuring NLog because we want to use a log file
+            // with the name which is the same as the name of the input file but with extension .log.
+            string logFileName = Path.ChangeExtension(inputFileName, ".log");
 
+            // Delete a possible log from the previous program run.
+            File.Delete(logFileName);
+
+            // Configure NLog.
+            ConfigureNLog(logFileName);
+
+            // Log message: we have managed to open the file!.
+            logger.Info("Opened {0} for reading data.", inputFileName);
+
+            // Create a parser.
             URLParser parser = new URLParser(line => URLData.Parse(line), data => data.ToXElement());
 
-            logger.Info("Initialized {0} as parser.", parser);
+            // Log message: we have created a parser!
+            logger.Info("Initialized {0} as the parser.", parser);
 
+            // Try to parse the file.
+            // xroot is the (root) <urladdresses> XElement.
             XElement xroot = null;
             using (reader)
             {
@@ -66,29 +90,34 @@ namespace URLParser
                 }
                 catch (Exception exc)
                 {
-                    Console.WriteLine("Cannot read from {0}", args[0]);
+                    Console.WriteLine("Cannot read from {0}", inputFileName);
                     Console.WriteLine(exc.Message);
-                    logger.Fatal(exc, "Cannot read from {0}", args[0]);
+                    logger.Fatal(exc, "Cannot read from {0}", inputFileName);
                     return INPUT_FILE_READ_FAILURE;
                 }
             }
 
+            // Log message: we have parsed the file!
             logger.Info("Parsed to XEelement.");
 
+            // Try to open the output file.
             StreamWriter writer = null;
             try
             {
-                writer = new StreamWriter(args[1]);
+                writer = new StreamWriter(outputFileName);
             }
             catch (Exception exc)
             {
-                Console.WriteLine("Cannot open " + args[1]);
+                Console.WriteLine("Cannot open {0}", outputFileName);
                 Console.WriteLine(exc);
+                logger.Fatal(exc, "Cannot open {0}", outputFileName);
                 return OUTPUT_FILE_OPEN_FAILURE;
             }
 
-            logger.Info("Openned {0} for writing data.", args[1]);
+            // Log message: we have opened the output file!
+            logger.Info("Openned {0} for writing data.", outputFileName);
 
+            // Write a string representation of xroot to the output file.
             using (writer)
             {
                 try
@@ -100,15 +129,17 @@ namespace URLParser
                 }
                 catch (Exception exc)
                 {
-                    Console.WriteLine("Cannot write to {0}", args[1]);
+                    Console.WriteLine("Cannot write to {0}", outputFileName);
                     Console.WriteLine(exc.Message);
-                    logger.Fatal("Cannot write to {0}", args[1]);
+                    logger.Fatal(exc, "Cannot write to {0}", outputFileName);
                     return OUTPUT_FILE_WRITE_FAILURE;
                 }
             }
 
+            // Log message: we have written to the output file!
             logger.Info("Wrote data to {0}.", args[1]);
 
+            // Make the program wait until NLog finishes up.
             LogManager.Shutdown();
 
             Console.WriteLine("Done.");
