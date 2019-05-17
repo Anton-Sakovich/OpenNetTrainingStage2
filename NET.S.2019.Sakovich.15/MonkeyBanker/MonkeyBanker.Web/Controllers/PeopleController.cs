621fdeef152ecda1a16258c3c933a641d7f6e62f
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-17 19:28:57 +0300

    Add simple CRUD for Person

--- NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Web/Controllers/PeopleController.cs
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Web/Controllers/PeopleController.cs
@@ -27,5 +27,85 @@ namespace MonkeyBanker.Web.Controllers
 
             return View(model);
         }
+
+        public ActionResult Edit(int id)
+        {
+            Person retrievedPerson = this.CrudToPeople.Read(id);
+
+            if (retrievedPerson != null)
+            {
+                return View(retrievedPerson);
+            }
+            else
+            {
+                return this.RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Person editedPerson)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(editedPerson);
+            }
+
+            this.CrudToPeople.Update(editedPerson);
+
+            return this.RedirectToAction("Index");
+        }
+
+        public ActionResult Create()
+        {
+            Person newPerson = new Person()
+            {
+                ID = 0
+            };
+
+            return this.View(newPerson);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person newPerson)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(newPerson);
+            }
+
+            if (this.CrudToPeople.Create(newPerson) == 1)
+            {
+                return this.RedirectToAction("Index");
+            }
+            else
+            {
+                return this.RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Person delPerson = this.CrudToPeople.Read(id);
+
+            if (delPerson == null)
+            {
+                return this.RedirectToAction("Index");
+            }
+
+            return this.View(delPerson);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Person delPerson)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(delPerson);
+            }
+
+            this.CrudToPeople.Delete(delPerson.ID);
+
+            return this.RedirectToAction("Index");
+        }
     }
 }
