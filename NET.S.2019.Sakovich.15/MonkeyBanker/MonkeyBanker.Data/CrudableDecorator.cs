7fb7848e1680417a97ee4e1c6efb4c19f7c0af21
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-19 21:26:10 +0300

    Add CrudableDecorator class

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Data/CrudableDecorator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyBanker.Data
+{
+    public class CrudableDecorator<T> : ICrudable<T>
+    {
+        protected readonly ICrudable<T> InnerCrudable;
+
+        public CrudableDecorator(ICrudable<T> innerCrudable)
+        {
+            this.InnerCrudable = innerCrudable;
+        }
+
+        public virtual int Create(T entity)
+        {
+            return this.InnerCrudable.Create(entity);
+        }
+
+        public virtual int Delete(int id)
+        {
+            return this.InnerCrudable.Delete(id);
+        }
+
+        public virtual T Read(int id)
+        {
+            return this.InnerCrudable.Read(id);
+        }
+
+        public virtual IEnumerable<T> Read()
+        {
+            return this.InnerCrudable.Read();
+        }
+
+        public virtual int Update(T entity)
+        {
+            return this.InnerCrudable.Update(entity);
+        }
+    }
+}
