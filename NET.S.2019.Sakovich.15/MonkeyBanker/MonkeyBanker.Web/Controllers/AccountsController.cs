5c31fd052e8501ce6e6903454a30b57356b51988
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-19 21:31:42 +0300

    Add Accounts/Index functionality

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Web/Controllers/AccountsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MonkeyBanker.Data;
+using MonkeyBanker.Entities;
+
+namespace MonkeyBanker.Web.Controllers
+{
+    public class AccountsController : CrudController<Account>
+    {
+        protected ICrudable<Person> crudToPeople;
+
+        public AccountsController(ICrudable<Account> crud, ICrudable<Person> crudToPeople)
+            : base(crud)
+        {
+            this.crudToPeople = crudToPeople;
+
+            bool needReadPeopleManually = true;
+
+            if (this.crud is IRelatedCrudable<Account> relatedCrud)
+            {
+                needReadPeopleManually = !relatedCrud.IsEager;
+            }
+
+            if (needReadPeopleManually)
+            {
+                this.crud = new ManualEagerCrudableAccounts(this.crud, this.crudToPeople, true);
+            }
+        }
+    }
+}
