1a8c4845f15fe3b24ed4e9700c7c6a482e804796
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-19 19:17:45 +0300

    Add IncrementAccountIdFactory to a new project...
    MonkeyBanker.Services.IdFactories

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Services.IdFactories/IncrementAccountIdFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonkeyBanker.Entities;
+
+namespace MonkeyBanker.Services.IdFactories
+{
+    public class IncrementAccountIdFactory : IIdFactory<Account>
+    {
+        private readonly IQueryable<Account> entities;
+
+        public IncrementAccountIdFactory(IQueryable<Account> entities)
+        {
+            this.entities = entities;
+        }
+
+        public void GenerateId(Account acc)
+        {
+            acc.ID = this.entities.Select(a => a.ID).Max() + 1;
+        }
+    }
+}
