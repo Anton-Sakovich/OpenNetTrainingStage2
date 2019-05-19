91fd23c2a32400c868aee63cc9859ea34c91c6ad
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-19 21:25:39 +0300

    Add IRelatedCrudable interface

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Data/IRelatedCrudable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyBanker.Data
+{
+    public interface IRelatedCrudable<T> : ICrudable<T>
+    {
+        bool IsEager { get; }
+    }
+}
