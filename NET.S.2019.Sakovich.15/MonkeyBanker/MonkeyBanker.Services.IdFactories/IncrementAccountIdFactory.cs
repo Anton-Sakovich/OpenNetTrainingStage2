bb0b9e7f90f56e8840746832b0d475cfbbacb773
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-20 16:27:15 +0300

    Reduce IncrementAccountIdFactory to a simple ...
    load all and increment variant (without retrieving IDs from an
    IQuerable<T>)

--- NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Services.IdFactories/IncrementAccountIdFactory.cs
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Services.IdFactories/IncrementAccountIdFactory.cs
@@ -4,21 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MonkeyBanker.Entities;
+using MonkeyBanker.Data;
 
 namespace MonkeyBanker.Services.IdFactories
 {
     public class IncrementAccountIdFactory : IIdFactory<Account>
     {
-        private readonly IQueryable<Account> entities;
+        private readonly ICrudable<Account> entities;
 
-        public IncrementAccountIdFactory(IQueryable<Account> entities)
+        public IncrementAccountIdFactory(ICrudable<Account> entities)
         {
             this.entities = entities;
         }
 
         public void GenerateId(Account acc)
         {
-            acc.ID = this.entities.Select(a => a.ID).Max() + 1;
+            acc.ID = this.entities.Read().Select(a => a.ID).Max() + 1;
         }
     }
 }
