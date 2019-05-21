797b89060e16fe8250e13101472b7c348eab22a4
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-21 18:48:23 +0300

    Add CrudDetailsViewModel

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Web/Models/CrudDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MonkeyBanker.Web.Models
+{
+    public class CrudDetailsViewModel<TEntity>
+    {
+        public TEntity Entity { get; set; }
+    }
+}
