8eb6e6670ed269f6d456ed435c4a7f5c3687c3c6
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-21 18:48:07 +0300

    Add CrudFormViewModel

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Web/Models/CrudFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MonkeyBanker.Web.Models
+{
+    public class CrudFormViewModel<TEntity>
+    {
+        public TEntity Entity { get; set; }
+
+        public string ButtonLabel { get; set; }
+    }
+}
