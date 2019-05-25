0188fe8a0818053b1d4edd4038fe00012a997ed9
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-25 21:42:22 +0300

    Add ajax-based deposit link to Accounts/Index

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Web/Controllers/TransactionsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MonkeyBanker.Entities;
+using MonkeyBanker.Services;
+using MonkeyBanker.Web.Models.Transactions;
+
+namespace MonkeyBanker.Web.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        protected DepositManager DepositManager;
+
+        protected WithdrawalManager WithdrawalManager;
+
+        public TransactionsController(DepositManager depositManager, WithdrawalManager withdrawalManager)
+        {
+            this.DepositManager = depositManager;
+
+            this.WithdrawalManager = withdrawalManager;
+        }
+
+        [HttpPost]
+        public ActionResult Deposit(decimal? sum)
+        {
+            if (sum == null)
+            {
+                return this.PartialView(new DepositViewModel() { Message = "Error" });
+            }
+
+            return this.PartialView(new DepositViewModel() { Message = $"The sum is {sum}" });
+        }
+    }
+}
---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Web/Models/Transactions/DepositViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MonkeyBanker.Web.Models.Transactions
+{
+    public class DepositViewModel
+    {
+        public string Message { get; set; }
+    }
+}
