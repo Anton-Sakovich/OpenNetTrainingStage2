7d37f48e02108ec702a7193184258eb606cafbbf
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-26 20:23:02 +0300

    Make deposit link actually deposit money and ...
    update the corresponding Account in the Index's table

--- NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Web/Controllers/TransactionsController.cs
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Web/Controllers/TransactionsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MonkeyBanker.Data;
 using MonkeyBanker.Entities;
 using MonkeyBanker.Services;
 using MonkeyBanker.Web.Models.Transactions;
@@ -15,22 +16,34 @@ namespace MonkeyBanker.Web.Controllers
 
         protected WithdrawalManager WithdrawalManager;
 
-        public TransactionsController(DepositManager depositManager, WithdrawalManager withdrawalManager)
+        protected ICrudable<Account> Accounts;
+
+        public TransactionsController(DepositManager depositManager, WithdrawalManager withdrawalManager, ICrudable<Account> accounts)
         {
             this.DepositManager = depositManager;
 
             this.WithdrawalManager = withdrawalManager;
+
+            this.Accounts = accounts;
         }
 
         [HttpPost]
-        public ActionResult Deposit(decimal? sum)
+        public ActionResult Deposit(int id, decimal sum)
         {
-            if (sum == null)
-            {
-                return this.PartialView(new DepositViewModel() { Message = "Error" });
-            }
+            Account accountToUpdate = this.Accounts.Read(id);
 
-            return this.PartialView(new DepositViewModel() { Message = $"The sum is {sum}" });
+            this.DepositManager.UpdateBalance(accountToUpdate, sum);
+
+            this.Accounts.Update(accountToUpdate);
+
+            DepositViewModel model = new DepositViewModel()
+            {
+                IsSuccess = true,
+                Message = "Success",
+                Account = accountToUpdate
+            };
+
+            return this.PartialView(model);
         }
     }
 }
--- NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Web/Models/Transactions/DepositViewModel.cs
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Web/Models/Transactions/DepositViewModel.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MonkeyBanker.Entities;
 
 namespace MonkeyBanker.Web.Models.Transactions
 {
     public class DepositViewModel
     {
+        public bool IsSuccess { get; set; }
+
         public string Message { get; set; }
+
+        public Account Account { get; set; }
+
+        public string IsSuccessString()
+        {
+            return this.IsSuccess ? "true" : "false";
+        }
     }
 }
