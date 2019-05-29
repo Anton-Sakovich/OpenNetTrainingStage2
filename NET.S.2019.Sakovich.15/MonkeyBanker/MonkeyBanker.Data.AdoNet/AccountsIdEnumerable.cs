2ad7dc44c090cc510821c90223f17926cab3de7b
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-05-29 15:41:21 +0300

    Add AccountsIdEnumerable
    AccountsIdEnumerable is a MonkeyBanker.Data.AdoNet implementation of
    IEnumerable<int> required for IncrementAccountIdFactory of
    MonkeyBanker.Services.IdFactories.

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Data.AdoNet/AccountsIdEnumerable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyBanker.Data.AdoNet
+{
+    public class AccountsIdEnumerable : IEnumerable<int>
+    {
+        protected DbProviderFactory Factory;
+
+        protected string ConnectionString;
+
+        public AccountsIdEnumerable(DbProviderFactory factory, string connectionString)
+        {
+            this.Factory = factory;
+
+            this.ConnectionString = connectionString;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            using (DbConnection connection = this.Factory.CreateConnection())
+            {
+                connection.ConnectionString = this.ConnectionString;
+
+                using (DbCommand command = this.Factory.CreateCommand())
+                {
+                    command.CommandText = "SELECT ID FROM Accounts;";
+                    command.Connection = connection;
+
+                    connection.Open();
+
+                    using (DbDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            yield return reader.GetInt32(0);
+                        }
+                    }
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
