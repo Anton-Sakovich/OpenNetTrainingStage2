ef6443b92812fa8e99a9748b4361bed1e66c0358
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-06-04 17:23:11 +0300

    Add IDbConnectionWrapper

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Data.AdoNet/IDbConnectionWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyBanker.Data.AdoNet
+{
+    public interface IDbConnectionWrapper : IDbConnection
+    {
+        IDbConnection ConnectionBase { get; }
+    }
+}
