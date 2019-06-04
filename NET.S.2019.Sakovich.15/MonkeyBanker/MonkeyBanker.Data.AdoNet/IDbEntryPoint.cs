8ae2a346bf6d6c65993cd06983559667097221c9
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-06-04 18:01:14 +0300

    Add IDbEntryPoint as a substitute for ..
    DbProviderFactory

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Data.AdoNet/IDbEntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyBanker.Data.AdoNet
+{
+    public interface IDbEntryPoint
+    {
+        IDbConnectionWrapper CreateConnection();
+
+        IDbCommand CreateCommand();
+
+        IDbDataParameter CreateParameter();
+    }
+}
