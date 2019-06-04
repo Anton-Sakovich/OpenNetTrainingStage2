4c7fae01f924442db69b1bfd5b2a856afd20ed6c
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-06-04 17:45:10 +0300

    Add SQLiteEntryPoint
    SQLiteEntryPoint is System.Data.SQLite based implementation of
    IDbEntryPoint factory.

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.ServiceResolver/SQLite/SQLiteEntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonkeyBanker.Data.AdoNet;
+
+namespace MonkeyBanker.ServiceResolver.SQLite
+{
+    public class SQLiteEntryPoint : IDbEntryPoint
+    {
+        public IDbCommand CreateCommand()
+        {
+            return new SQLiteCommand();
+        }
+
+        public IDbConnectionWrapper CreateConnection()
+        {
+            return new SQLiteConnectionHooked(new SQLiteConnection());
+        }
+
+        public IDbDataParameter CreateParameter()
+        {
+            return new SQLiteParameter();
+        }
+    }
+}
