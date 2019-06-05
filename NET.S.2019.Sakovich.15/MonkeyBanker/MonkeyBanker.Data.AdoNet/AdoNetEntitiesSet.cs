e6058965e425d3d513cee0d8a2c9a1eadb88cc27
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-06-05 17:22:03 +0300

    Add AdoNetEntitiesSet

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Data.AdoNet/AdoNetEntitiesSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyBanker.Data.AdoNet
+{
+    public class AdoNetEntitiesSet<TEntity> : IEntitiesSet<TEntity>
+    {
+        public AdoNetEntitiesSet(ICrudable<TEntity> crud)
+        {
+            this.Crud = crud;
+        }
+
+        public ICrudable<TEntity> Crud { get; }
+    }
+}
