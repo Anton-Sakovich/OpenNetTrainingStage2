694576cb54709899f2b89d05028ae110783318a7
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-06-05 17:20:19 +0300

    Introduce Repository
    Repository is a façade for MonkeyBanker.Data. It encapsulates all sets
    of entities handled by the DAL.

---
+++ NET.S.2019.Sakovich.15/MonkeyBanker/MonkeyBanker.Data/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonkeyBanker.Entities;
+
+namespace MonkeyBanker.Data
+{
+    public class Repository
+    {
+        public Repository(IRepositoryInitializer initializer)
+        {
+            initializer.Initialize(this);
+        }
+
+        public IEntitiesSet<Person> People { get; set; }
+
+        public IEntitiesSet<Account> Accounts { get; set; }
+    }
+}
